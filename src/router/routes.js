4c16d30e728dff82853e819cdccaeb57851ca3dd
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-04-25 10:52:11 +0200

    no lazy-loading router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,32 @@
+import PageHome from "pages/PageHome.vue"
+import PageAbout from "pages/PageAbout.vue"
+import PageProjects from "pages/PageProjects.vue"
+import PageContact from "pages/PageContact.vue"
+
 const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/PageHome.vue") },
+      { 
+        path: "", 
+        // component: () => import("pages/PageHome.vue"), 
+        component: PageHome 
+      },
       {
         path: "/about",
-        component: () => import("pages/PageAbout.vue")
+        // component: () => import("pages/PageAbout.vue")
+        component: PageAbout
       },
       {
         path: "/projects",
-        component: () => import("pages/PageProjects.vue")
+        // component: () => import("pages/PageProjects.vue")
+        component: PageProjects
       },
       {
         path: "/contact",
-        component: () => import("pages/PageContact.vue")
+        // component: () => import("pages/PageContact.vue")
+        component: PageContact
       }
     ]
   },
